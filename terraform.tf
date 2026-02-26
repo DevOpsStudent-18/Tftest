62c78ac7fdb5d9750ed8260364dd12e285f37e27
testuser <test@mail.com>
2026-02-26 09:40:47 +0530

    updated random resource

--- terraform.tf
+++ terraform.tf
@@ -1,10 +1,10 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.92"
+      source  = "hashicorp/random"
+      version = "~> 3.0"
     }
   }
 
-  required_version = ">= 1.2"
+  required_version = "~> 1.14"
 }
