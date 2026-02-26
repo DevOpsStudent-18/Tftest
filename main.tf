87e95b4e7abe7d076c3f86e9060d6b9e5c25fbab
testuser <test@mail.com>
2026-02-26 10:00:01 +0530

    test azure

--- main.tf
+++ main.tf
@@ -1,3 +1,48 @@
-resource "random_pet" "pet_name" {
-  length = var.length
+
+# Azure provider
+provider "azurerm" {
+  features {}
+}
+
+# Resource group
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+# Storage account (required for App Service)
+resource "azurerm_storage_account" "storage" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+# App Service Plan
+resource "azurerm_app_service_plan" "app_service_plan" {
+  name                = var.app_service_plan_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku {
+    tier = "Basic"
+    size = "B1"
+  }
+}
+
+# App Service (Web App)
+resource "azurerm_app_service" "webapp" {
+  name                = var.webapp_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.app_service_plan.id
+}
+
+# Key Vault
+resource "azurerm_key_vault" "kv" {
+  name                = var.key_vault_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tenant_id           = var.tenant_id
+  sku_name            = "standard"
 }
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
-output "pet_name" {
-  value = random_pet.pet_name.id
+
+output "app_service_default_site_hostname" {
+  value = azurerm_app_service.webapp.default_site_hostname
+}
+
+output "key_vault_uri" {
+  value = azurerm_key_vault.kv.vault_uri
 }
