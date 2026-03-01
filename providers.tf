fdecae10e070a02a793c9cce26eafe773654a47f
testuser <test@mail.com>
2026-03-01 19:45:01 +0530

    udpates

---
+++ providers.tf
@@ -0,0 +1,7 @@
+provider "azurerm" {
+  features {}
+}
+
+provider "azuread" {
+  # Configuration can be added if needed
+}
---
+++ versions.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = "~> 1.14.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.62.0"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = ">= 2.47.0"
+    }
+  }
+}
